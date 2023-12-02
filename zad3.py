c6b85c5b0bfb77c3bc9cb950e7ea4b8571044218
JuliaPozniak5 <152688158+JuliaPozniak5@users.noreply.github.com>
2023-12-02 11:09:24 +0100

    Add files via upload

---
+++ zad3.py
@@ -0,0 +1,14 @@
+#komentarz wierszowy
+""" napisz progrm, ktory po podaniu licznika liczbnika a i mianownika b
+poda na jaką liczbę mieszaną
+ należy zamienic ta liczbe mieszana przy drukowaniu"""
+ a=5
+ b=2
+ c=a//b
+ l=a%b #licznik 
+ m=m #mianownik
+
+# % reszta z dzielenia
+ print(c)
+ print(l)
+ print(m)
